845ed1cb0e54dd89a5d9501d68de74851bc74221
David Morton <davidmortonmail@gmail.com>
2017-04-08 00:12:46 +0100

    Added SSL to DB url

--- src/main/java/com/bitbosh/DropwizardHeroku/MainConfiguration.java
+++ src/main/java/com/bitbosh/DropwizardHeroku/MainConfiguration.java
@@ -13,41 +13,49 @@ import io.dropwizard.db.DataSourceFactory;
 
 public class MainConfiguration extends Configuration {
 
+  @Valid
+  @NotNull
+  private DataSourceFactory database = new DataSourceFactory();
 
-	@Valid
-	@NotNull
-	private DataSourceFactory database = new DataSourceFactory();
+  /**
+   * A getter for the database factory.
+   *
+   * @return An instance of database factory deserialized from the configuration
+   *         file passed as a command-line argument to the application.
+   * @throws URISyntaxException
+   */
+  @JsonProperty("database")
+  public DataSourceFactory getDataSourceFactory() throws URISyntaxException {
 
-	/**
-     * A getter for the database factory.
-     *
-     * @return An instance of database factory deserialized from the
-     * configuration file passed as a command-line argument to the application.
-	 * @throws URISyntaxException 
-     */
-    @JsonProperty("database")
-    public DataSourceFactory getDataSourceFactory() throws URISyntaxException {
-    	
-    	// Overwrite the value read from the database property of the config.yml file 
-    	// and use the value provided by Heroku environment.   
-    	URI dbUri = new URI(System.getenv("DATABASE_URL"));
+    // Overwrite the value read from the database property of the config.yml
+    // file and use the value provided by Heroku environment.
+    //
+    // When running the app locally using `heroku local` the remote postgres
+    // db url needs to be added to the .env file.
+    // Run `heroku config` and copy `DATABASE_URL: <database-url>` into the
+    // .env file.
+    //
+    // For reference:
+    // https://devcenter.heroku.com/articles/connecting-to-relational-databases-on-heroku-with-java#connecting-to-a-database-remotely
+    URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
 
-        this.database.setUser(username);
-        this.database.setPassword(password);
-        this.database.setUrl(dbUrl);
-        return database;
-    }
-    
-    /**
-     * Needed to set the database factory when the config.yml file is loaded.
-     * @param dataSourceFactory
-     */
-    @JsonProperty("database")
-    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
-        this.database = dataSourceFactory;
-    }
+    this.database.setUser(username);
+    this.database.setPassword(password);
+    this.database.setUrl(dbUrl);
+    return database;
+  }
+
+  /**
+   * Needed to set the database factory when the config.yml file is loaded.
+   * 
+   * @param dataSourceFactory
+   */
+  @JsonProperty("database")
+  public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+    this.database = dataSourceFactory;
+  }
 }
